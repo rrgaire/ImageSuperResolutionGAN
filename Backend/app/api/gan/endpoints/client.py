e940d49a4725b8887a5fdd96765b43245736e668
Rebati Raman Gaire <073bct533.rebati@pcampus.edu.np>
2020-09-14 17:56:01 +0545

    CORS Support Added

--- Backend/app/api/gan/endpoints/client.py
+++ Backend/app/api/gan/endpoints/client.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import misc
 from PIL import Image
 import json
+import jsonpickle
 
 from flask import request
 from flask_restplus import Resource
@@ -35,24 +36,27 @@ class GanPrediction(Resource):
                 })
     @ns.expect(upload_parser)
     def post(self):
+
         try:
+
             image_file = request.files[UPLOAD_KEY]
             image_name = image_file.filename
             image_file = Image.open(image_file)
+
             image_dict = {'image': image_file, 'image_name': image_name}
 
-          
+
         except Exception as inst:
             return {'message': 'something wrong with incoming request. ' +
                                'Original message: {}'.format(inst)}, 400
 
-        
-        
+
+
         try:
             results = make_prediction(image_dict)
-            
-            #results_json = [{'digit': res[0], 'probability': res[1]} for res in results]
-            return {'prediction_result': 'Successfully upscaled and saved in the Output_Image direcory'}, 200
+            results = jsonpickle.encode(results)
+            # print(results)
+            return {'prediction_result': results}, 200
 
         except Exception as inst:
             return {'message': 'internal error: {}'.format(inst)}, 500
--- Backend/app/app.py
+++ Backend/app/app.py
@@ -5,12 +5,15 @@ import settings
 import utils
 from flask import Flask, Blueprint
 from flask_restplus import Resource, Api
+from flask_cors import CORS
+
 
 from api.restplus import api
 from api.gan.endpoints.client import ns as gan_client_namespace
 
 # create Flask application
 application = Flask(__name__)
+CORS(application)
 
 # load logging configuration and create log object
 logging.config.fileConfig(os.path.join(os.path.dirname(__file__), 'logging.conf'))
@@ -18,7 +21,7 @@ log = logging.getLogger(__name__)
 
 
 def __get_flask_server_params__():
-  
+
     server_name = utils.get_env_var_setting('FLASK_SERVER_NAME', settings.DEFAULT_FLASK_SERVER_NAME)
     server_port = int(utils.get_env_var_setting('FLASK_SERVER_PORT', settings.DEFAULT_FLASK_SERVER_PORT))
     flask_debug = utils.get_env_var_setting('FLASK_DEBUG', settings.DEFAULT_FLASK_DEBUG)
@@ -28,7 +31,7 @@ def __get_flask_server_params__():
     return server_name, server_port, flask_debug
 
 def configure_app(flask_app):
-   
+
     flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
     flask_app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VALIDATE
     flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER
@@ -36,7 +39,7 @@ def configure_app(flask_app):
 
 
 def initialize_app(flask_app):
-    
+
     blueprint = Blueprint('tf_api', __name__, url_prefix='/tf_api')
 
     configure_app(flask_app)
