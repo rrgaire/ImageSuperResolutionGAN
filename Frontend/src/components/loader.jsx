802a49569d2979e4ff0358792e90b6c25cce9cbd
Ronast <sronast@gmail.com>
2020-10-15 12:48:45 +0545

    Added image loding animation

---
+++ Frontend/src/components/loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Loader = () => {
+    return (
+        <div className="ring">
+            Loading
+            <span className="ring-span"></span>
+        </div>
+    );
+}
+ 
+export default Loader;
