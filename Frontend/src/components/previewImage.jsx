4a681f2bcda0000e932085921878abaeb879ec02
ashimsharma10 <073bct508.ashim@pcampus.edu.np>
2020-10-24 21:10:32 +0545

    added hover on zoom using react-image-magnify

--- Frontend/src/components/previewImage.jsx
+++ Frontend/src/components/previewImage.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import ReactImageMagnify from "react-image-magnify";
 import Loader from "./loader";
 
 import FileContext from "../context/fileContext";
@@ -14,15 +15,46 @@ const PreviewImage = () => {
     <div className="image-preview-box">
       <div className="image-container-wrapper">
         <div className="image-container">
-          <img src={fileUrl} alt="Your Img" />
+          {/* <img src={fileUrl} alt="Your Img" /> */}
+          <ReactImageMagnify
+            {...{
+              smallImage: {
+                alt: "Wristwatch by Ted Baker London",
+                isFluidWidth: true,
+                src: fileUrl,
+              },
+              largeImage: {
+                src: fileUrl,
+                width: 1200,
+                height: 1800,
+              },
+              enlargedImagePosition: "over",
+            }}
+          />
         </div>
       </div>
       <div className="image-container-wrapper">
+      <div className="image-container">
         {isFileUpscaled && (
-          <div className="image-container">
-            <img src={`data:image/jpeg;base64,${upscaledData}`} alt="" />
-          </div>
+          <ReactImageMagnify
+          {...{
+            smallImage: {
+              alt: "Wristwatch by Ted Baker London",
+              isFluidWidth: true,
+              src: `data:image/jpeg;base64,${upscaledData}`,
+            },
+            largeImage: {
+              src: `data:image/jpeg;base64,${upscaledData}`,
+              width: 1200,
+              height: 1800,
+            },
+            enlargedImagePosition: "over",
+          }}
+        />
+            // <img src={`data:image/jpeg;base64,${upscaledData}`} alt="" />
+          
         )}
+        </div>
 
         {!isFileUpscaled && isLoading && <Loader />}
 
